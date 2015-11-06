a908f4c7da0a33d037555a0c025e843c6584574f
T.J. Mazeika <t@mazeika.me>
2015-11-06 13:14:38 -0500

    Optimize imports

--- src/main/java/me/mazeika/uconfig/UConfig.java
+++ src/main/java/me/mazeika/uconfig/UConfig.java
@@ -7,7 +7,10 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 class UConfig extends Config
 {
